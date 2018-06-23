e542e97f7d5390b52242cac771433c23bb858172
shivapbhusal <civabhusal@gmail.com>
2018-06-23 00:10:13 -0400

    update script

--- adjustBrackets.py
+++ adjustBrackets.py
@@ -1,22 +1,32 @@
 '''
 Python Script to reverse chars inside a bracket.
+
+This is a recursive solution.
 '''
 
 def reverse(myList, recent_left_bracket, current):
     '''
-        check this.
+    Inputs: A list, start index of left bracket, index of right bracket
+    Returns: The same list with chars inside bracket in reversed order. 
     '''
-    i = recent_left_bracket
-    j = current
-    while (i!=j):
+    i = recent_left_bracket+1
+    j = current-1
+    while (i-j)>1:
         temp = myList[i]
         myList[i] = myList[j]
         myList[j] = temp
         i = i + 1
         j = j-1
+    temp = myList[i]
+    myList[i] = myList[j]
+    myList[j] = temp
     return myList
 
 def reverseParentheses(s):
+    '''
+    Gets the index of first right parentheses and the most recent left parentheses. 
+    The process continues recursively until the string doesn't contain any parentheses.
+    '''
     if "(" not in s or ")" not in s:
         return s
     else:
@@ -32,6 +42,8 @@ def reverseParentheses(s):
     del myList[i]
     del myList[recent_left_bracket]
     return reverseParentheses("".join(map(str, myList)))
-    i = i+1
 
-print(reverseParentheses("Hello((abc)(One)xyz)"))
+// tests
+print(reverseParentheses("Hello(ab)xyz"))
+print(reverseParentheses("Hello(ab)(cd)xyz"))
+print(reverseParentheses("Hello(wxy)(a)"))
